10365a21be4a12eb6c73f6a01319ad5641968c67
Arnaud Karambizi <arnaudkarambizi4@gmail.com>
2019-12-12 21:27:59 -0500

    added

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,9 +1,4 @@
 const mynav = $(".mynav")
     .hide()
-    .slideDown(2000);
-// const about = $("#mainpage")
-//     .hide()
-//     .fadeIn(5000);
-// const images = $(".card")
-//     .hover()
-//     .fadeOut(6000);
+    .slideDown(1000);
+ 
